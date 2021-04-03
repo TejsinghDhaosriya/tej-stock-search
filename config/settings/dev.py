900cce4a6c7cd0a489d98e2b4d2591933b5f35ae
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-04-03 16:06:01 +0530

    env

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -2,7 +2,7 @@
 
 from .base import *
 DEBUG = True
-WSGI_APPLICATION = 'config.wsgi.prod.application'
+WSGI_APPLICATION = 'config.wsgi.dev.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -16,24 +16,5 @@ ALLOWED_HOSTS = ["*"]
 
 
 
-
-# import dj_database_url
-# from .base import *
-# DEBUG = True
-# WSGI_APPLICATION = 'config.wsgi.prod.application'
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'root',
-#         'USER': 'root',
-#         'PASSWORD': 'root',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ALLOWED_HOSTS = ["tej-todo-backend.herokuapp.com"]
