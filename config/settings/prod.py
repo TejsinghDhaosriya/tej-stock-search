aa014433b54f3857e9db676e5a6e4741aa89d8d5
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-04-03 15:57:35 +0530

    deploy

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -17,4 +17,6 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-ALLOWED_HOSTS = ["tej-todo-backend.herokuapp.com"]
+
+
+ALLOWED_HOSTS = ["*"]
--- frontend/src/components/stock/api.js
+++ frontend/src/components/stock/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:8000"
+const baseURL = "https://tej-stock.herokuapp.com"
+// const baseURL = "http://127.0.0.1:8000"
 export const getStocksListing = (params) => axios.get(`${baseURL}/api/stock`, { params });
 export const getStocksDetail = (id) => axios.get(`${baseURL}/api/stock/${id}/`);
